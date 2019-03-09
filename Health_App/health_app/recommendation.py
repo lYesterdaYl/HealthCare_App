082640c5c2013a19bca9bad83a59f31e8ca54d5d
yyhtudou <34073844+yyhtudou@users.noreply.github.com>
2019-03-09 12:47:21 -0800

    Add files via upload

--- Health_App/health_app/recommendation.py
+++ Health_App/health_app/recommendation.py
@@ -37,7 +37,7 @@ class recommendation:
         user_index = self.get_user_index(self.user_id,users)
         item_index = collaborative_filtering(ratings).recommend_item(user_index)
         item_id = items[item_index]
-        return item_id
+        return {self.prefer:item_id}
 
     def get_user_index(self,userid,users):
         result = 0
@@ -72,4 +72,3 @@ class recommendation:
             for user in ratings_dict[item]:
                 ratings[user_dict[user]][item_dict[item]] = ratings_dict[item][user]
         return (users,items,ratings)
-
